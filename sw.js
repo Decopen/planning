456bc015dbeae6a78f570a0c7c8e2a02faf40270
26pen <26pen2004@gmail.com>
2026-07-13 09:45:17 +0200

    first commit

---
+++ sw.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'planning-control-v1';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Installation : met en cache les fichiers essentiels de l'application
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(CORE_ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activation : supprime les anciens caches (versions précédentes)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
+});
+
+// Fetch : stratégie "cache d'abord, réseau en secours" pour les fichiers de l'app,
+// et "réseau d'abord" pour tout le reste (CDN jsPDF, polices Google Fonts, etc.)
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  const isSameOrigin = url.origin === self.location.origin;
+
+  if (isSameOrigin) {
+    event.respondWith(
+      caches.match(event.request).then((cached) => {
+        if (cached) return cached;
+        return fetch(event.request).then((response) => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          return response;
+        }).catch(() => cached);
+      })
+    );
+  } else {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  }
+});
